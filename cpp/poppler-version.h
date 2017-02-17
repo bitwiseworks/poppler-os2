5788aecd5e722ca2d09e73d0f320ee654140a2d5
Silvan Scherrer <silvan.scherrer@aroa.ch>
2017-02-17 12:43:15 +0000

    poppler: update vendor to version 0.52.0
    git-svn-id: http://svn.netlabs.org/repos/ports/poppler/vendor/current@2026 8d6eb4b1-0faa-de11-b929-00e081727c95

--- cpp/poppler-version.h
+++ cpp/poppler-version.h
@@ -21,9 +21,9 @@
 
 #include "poppler-global.h"
 
-#define POPPLER_VERSION "0.51.0"
+#define POPPLER_VERSION "0.52.0"
 #define POPPLER_VERSION_MAJOR 0
-#define POPPLER_VERSION_MINOR 51
+#define POPPLER_VERSION_MINOR 52
 #define POPPLER_VERSION_MICRO 0
 
 namespace poppler
