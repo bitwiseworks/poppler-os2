4c415a37235866ab34faceec25e04bc81440a1af
SilvanScherrer <silvan.scherrer@aroa.ch>
2020-01-28 09:16:18 +0100

    use the right order for the exports

--- glib/poppler-macros.h
+++ glib/poppler-macros.h
@@ -24,10 +24,10 @@
  */
 #if defined(_WIN32)
 #  define POPPLER_PUBLIC __declspec(dllexport)
-#elif defined(__GNUC__)
-#  define POPPLER_PUBLIC __attribute__((visibility("default"))) extern
 #elif defined(__OS2__)
 #  define POPPLER_PUBLIC __declspec(dllexport)
+#elif defined(__GNUC__)
+#  define POPPLER_PUBLIC __attribute__((visibility("default"))) extern
 #else
 #  define POPPLER_PUBLIC
 #endif
